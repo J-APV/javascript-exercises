331ac8ebe2ae0a318dd7e99c3ad6a1bc7f46dae5
Jose <104174192+J-APV@users.noreply.github.com>
2024-09-02 16:08:28 -0700

    Complete 1,2,3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    //car --> rac
+    let reversedString = '';
+    for(let i = 1; i < word.length; i++){
+        reversedString = word.slice(1, word.length).concat(word.at(i-1));
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
